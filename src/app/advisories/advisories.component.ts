66c7c2a9eedffc0c4cbcafff9a8d2347645bfd20
Vijay <gandevijay@live.com>
2018-02-01 21:28:46 -0500

    fist version push

---
+++ src/app/advisories/advisories.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IAdvisories } from './advisories';
+import { AdvisoriesService } from './advisories.service';
+
+@Component({
+  selector: 'pm-advisories',
+  templateUrl: './advisories.component.html',
+  styleUrls: ['./advisories.component.css']
+})
+export class AdvisoriesComponent implements OnInit {
+  
+    pageTitle: string = 'Advisories Detail';
+    errorMessage: string;
+    advisories: IAdvisories;
+  
+    constructor(private _route: ActivatedRoute,
+      private _router: Router,
+      private _advisoriesService: AdvisoriesService) { }
+            
+    ngOnInit(): void {
+      this._advisoriesService.getAdvisories()
+              .subscribe(advisories => 
+                  this.advisories = advisories,
+                  error => this.errorMessage = <any>error);
+  }
+  
+    onBack(): void {
+      this._router.navigate(['/advisories']);
+    }
+  
+  }
+  
---
+++ src/app/advisories/advisories.ts
@@ -0,0 +1,11 @@
+export interface IAdvisories {
+    advisoriesId: number;
+    title: string;
+    advisoryType: string;
+    effectiveDate: string;
+    xml: string;
+    productGroup: number;
+    product: string;
+    entityType: string;
+    actualStartDate: string;   
+}
